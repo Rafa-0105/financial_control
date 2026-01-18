4e4419ee0c04617b143cbdc011eda747e3de3d68
Rafael Gambin <rafaelgambinrocha@hotmail.com>
2026-01-17 22:04:38 -0300

    feat: register new routers in main application

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from routes.put.router_put import router as put_router
 from routes.delete.router_delete import router as delete_router
 from routes.post.create_login import router as login_router
 from routes.post.create_user import router as user_router
+from routes.batch.router_batch import router as batch_router
+from routes.analytics.router_analytics import router as analytics_router
+from routes.excel.router_excel import router as excel_router
 from database.connection_db import init_db
 
 app = FastAPI()
@@ -18,6 +21,9 @@ app.include_router(put_router)
 app.include_router(delete_router)
 app.include_router(login_router)
 app.include_router(user_router)
+app.include_router(batch_router)
+app.include_router(analytics_router)
+app.include_router(excel_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host="localhost", port=8000, reload=True)
